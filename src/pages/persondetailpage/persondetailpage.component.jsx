f6a2a977664c8d0aa123a6ebffc9e6a45ca09136
Jonathan Dueck <jdd612@gmail.com>
2020-03-07 11:33:25 -0700

    refactor persondetailpage

--- src/pages/persondetailpage/persondetailpage.component.jsx
+++ src/pages/persondetailpage/persondetailpage.component.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import PersonDetailForm from '../../components/people/person-detail-form';
 import PersonDetail from '../../components/people/person-detail.component';
@@ -8,87 +8,146 @@ import { db } from '../../config/Firebase';
 import Loader from '../../components/loader/loader.component';
 
 
-class PersonDetailPage extends React.Component {
+const PersonDetailPage = props => {
+	const [editMode, setEditMode] = useState(false);
+	const [person, setPerson] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
 
-	constructor(props) {
-		super(props);
+	const toggleState = (editMode, permission) => {
 
-		this.state = {
-			editMode: false,
-			person: {},
-			isLoading: false
-		}
-		this.toggleState = this.toggleState.bind(this);
-		this.refetchProfile = this.refetchProfile.bind(this);
 	}
 
-	componentDidMount() {
-		// Fetch this user from the backend, getting user id from address bar
-		this.setState({ isLoading: true })
-		db.collection('profiles').where("id", "==", this.props.match.params.id)
+	const refetchProfile = () => {
+
+	}
+
+
+	useState(() => {
+		setIsLoading(true);
+		db.collection('profiles').where("id", "==", props.match.params.id)
 			.get()
 			.then((querySnapshot) => {
 				querySnapshot.forEach((doc) => {
 					doc.data()
-					this.setState({ person: doc.data(), isLoading: false });
+					setPerson(doc.data());
+					setIsLoading(false);
+					// this.setState({ person: doc.data(), isLoading: false });
 				})
 			})
-	}
+	});
 
-	toggleState(editMode, permission) {
-		const profileWeAreVisiting = this.props.match.params.id;
-		const profileIdOfCurrentUser = localStorage.getItem('BuiltInSouthernUtah');
-		const ownProfile = Boolean(profileWeAreVisiting === profileIdOfCurrentUser);
-		if (ownProfile || Number(permission) === 3) {
-			this.setState({ editMode });
-		} else {
-			console.log("Must be admin or own this profile, in order to edit it.");
-			this.setState({ editMode: false })
-		}
-	}
 
-	refetchProfile(id) {
-		this.setState({ loading: true });
-		db.collection('profiles').where("id", "==", this.props.match.params.id)
-			.get()
-			.then((querySnapshot) => {
-				querySnapshot.forEach((doc) => {
-					doc.data()
-					this.setState({ person: doc.data(), isLoading: false });
-				})
-			})
-	}
+	if (isLoading === true) return <Loader />
+	const { hasProfile, title, displayName, headshotSrc, description, profileVisible, facebook, twitter, linkedin, github, portfolio } = person;
+	const id = props.match.params.id;
 
-	render() {
-		if (this.state.isLoading === true) return <Loader />
-		const { hasProfile, title, displayName, headshotSrc, description, profileVisible, facebook, twitter, linkedin, github, portfolio } = this.state.person;
-		const { editMode } = this.state;
-		const id = this.props.match.params.id;
-		return (
-			<Fragment>
-				<UserContext.Consumer>
-					{user => (
-						<PersonDetail
-							id={id}
-							title={title}
-							displayName={displayName}
-							headshotSrc={headshotSrc}
-							description={description}
-							editMode={this.toggleState}
-							permission={user.permission}
-							profileVisible={profileVisible}
-							facebook={facebook}
-							twitter={twitter}
-							linkedin={linkedin}
-							github={github}
-							portfolio={portfolio}
-						/>
-					)}
+	return (
+		<Fragment>
+			<UserContext.Consumer>
+				{user => (
+					<PersonDetail
+						id={id}
+						title={title}
+						displayName={displayName}
+						headshotSrc={headshotSrc}
+						description={description}
+						editMode={toggleState}
+						permission={user.permission}
+						profileVisible={profileVisible}
+						facebook={facebook}
+						twitter={twitter}
+						linkedin={linkedin}
+						github={github}
+						portfolio={portfolio}
+					/>
+				)}
 
-				</UserContext.Consumer>
-			</Fragment>
-		);
-	}
+			</UserContext.Consumer>
+		</Fragment>
+	)
 }
+
+// class PersonDetailPage extends React.Component {
+
+// 	constructor(props) {
+// 		super(props);
+
+// 		this.state = {
+// 			editMode: false,
+// 			person: {},
+// 			isLoading: false
+// 		}
+// 		this.toggleState = this.toggleState.bind(this);
+// 		this.refetchProfile = this.refetchProfile.bind(this);
+// 	}
+
+// 	componentDidMount() {
+// 		// Fetch this user from the backend, getting user id from address bar
+// 		this.setState({ isLoading: true })
+// 		db.collection('profiles').where("id", "==", this.props.match.params.id)
+// 			.get()
+// 			.then((querySnapshot) => {
+// 				querySnapshot.forEach((doc) => {
+// 					doc.data()
+// 					this.setState({ person: doc.data(), isLoading: false });
+// 				})
+// 			})
+// 	}
+
+// 	toggleState(editMode, permission) {
+// 		const profileWeAreVisiting = this.props.match.params.id;
+// 		const profileIdOfCurrentUser = localStorage.getItem('BuiltInSouthernUtah');
+// 		const ownProfile = Boolean(profileWeAreVisiting === profileIdOfCurrentUser);
+// 		if (ownProfile || Number(permission) === 3) {
+// 			this.setState({ editMode });
+// 		} else {
+// 			console.log("Must be admin or own this profile, in order to edit it.");
+// 			this.setState({ editMode: false })
+// 		}
+// 	}
+
+// 	refetchProfile(id) {
+// 		this.setState({ loading: true });
+// 		db.collection('profiles').where("id", "==", this.props.match.params.id)
+// 			.get()
+// 			.then((querySnapshot) => {
+// 				querySnapshot.forEach((doc) => {
+// 					doc.data()
+// 					this.setState({ person: doc.data(), isLoading: false });
+// 				})
+// 			})
+// 	}
+
+// 	render() {
+// if (this.state.isLoading === true) return <Loader />
+// const { hasProfile, title, displayName, headshotSrc, description, profileVisible, facebook, twitter, linkedin, github, portfolio } = this.state.person;
+// const { editMode } = this.state;
+// const id = this.props.match.params.id;
+// 		return (
+// 			<Fragment>
+// 				<UserContext.Consumer>
+// 					{user => (
+// 						<PersonDetail
+// 							id={id}
+// 							title={title}
+// 							displayName={displayName}
+// 							headshotSrc={headshotSrc}
+// 							description={description}
+// 							editMode={this.toggleState}
+// 							permission={user.permission}
+// 							profileVisible={profileVisible}
+// 							facebook={facebook}
+// 							twitter={twitter}
+// 							linkedin={linkedin}
+// 							github={github}
+// 							portfolio={portfolio}
+// 						/>
+// 					)}
+
+// 				</UserContext.Consumer>
+// 			</Fragment>
+// 		);
+// 	}
+// }
 
 export default withRouter(PersonDetailPage);
